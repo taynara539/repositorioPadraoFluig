1ab106e9cd8692885a5ca1516647154814e56233
taynara539 <taynara.kris@gmail.com>
2020-01-07 19:55:28 -0200

    Implementar

---
+++ forms/projeto1/js/script.js
@@ -0,0 +1,33 @@
+//Implementar
+/**
+ * Super Widget
+ * Autocomplete
+ * Back to Top
+ * Back to Bottom
+ * Buttons
+ * Calendar
+ * Collapse
+ * Continuous Scroll
+ * Copy
+ * Confirm, Alert and Error messages
+ * Datatable
+ * Dropdowns
+ * Desktop Notification
+ * Editable
+ * Loading
+ * Modals
+ * Message Page
+ * Password
+ * Periodical Executor
+ * Popover
+ * Right Bar
+ * Sliders
+ * Storage
+ * Switch button
+ * Time Interaction
+ * Toast
+ * Togglable tabs
+ * Tooltip
+ * Utilities
+ *
+ */
